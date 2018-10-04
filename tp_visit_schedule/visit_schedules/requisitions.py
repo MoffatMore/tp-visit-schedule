9c53955d5fa1ded41093f03bcf1e56c81bb0634c
moffatmore <mofenyimoffat@gmail.com>
2018-10-04 16:16:11 +0200

    updating visit schedule

--- tp_visit_schedule/visit_schedules/requisitions.py
+++ tp_visit_schedule/visit_schedules/requisitions.py
@@ -1,28 +1,16 @@
-from tp_labs import chemistry_alt_panel, chemistry_panel, csf_chemistry_panel
+from tp_labs import  chemistry_panel
 from edc_visit_schedule import FormsCollection, Requisition
 
 requisitions_prn = FormsCollection(
     Requisition(
         show_order=10,
         panel=chemistry_panel, required=False, additional=False),
-    Requisition(
-        show_order=20,
-        panel=chemistry_alt_panel, required=False, additional=False),
-    Requisition(
-        show_order=30,
-        panel=csf_chemistry_panel, required=False, additional=False),
     name='requisitions_prn')
 
 requisitions_d1 = FormsCollection(
     Requisition(
         show_order=10,
         panel=chemistry_panel, required=False, additional=False),
-    Requisition(
-        show_order=20,
-        panel=chemistry_alt_panel, required=False, additional=False),
-    Requisition(
-        show_order=30,
-        panel=csf_chemistry_panel, required=False, additional=False),
     name='requisitions_day1'
 )
 
@@ -30,12 +18,6 @@ requisitions_d3 = FormsCollection(
     Requisition(
         show_order=10,
         panel=chemistry_panel, required=False, additional=False),
-    Requisition(
-        show_order=20,
-        panel=chemistry_alt_panel, required=False, additional=False),
-    Requisition(
-        show_order=30,
-        panel=csf_chemistry_panel, required=False, additional=False),
     name='requisitions_day3'
 )
 
